ed7cba29a60a301215cebdab52edf8b929b08717
Ariel <ariel1242009@gmail.com>
2021-12-16 16:37:56 -0300

    checkpoint

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import ReactDOM from "react-dom";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './reset.css';
 import './style.css';
 import Header from './components/Header';
 import Catalogo from "./components/Catalogo/Catalogo";
 import Datas from "./components/Datas/Datas";
+import { useState } from "react";
 
 function App(){
+  const [idFilme, setIdFilme] = useState(null);
+
   return (
-    <>
-      <Header></Header>
-      <Catalogo></Catalogo>
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Catalogo setIdFilme={setIdFilme}/>}></Route>
+        <Route path="/datas" element={<Datas id={idFilme}/>}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
